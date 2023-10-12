500ecaab024a61a2ac798dc8ec6f249cdf5d539f
Al Amin Wicaksono <alaminwicaksono@gmail.com>
2023-10-12 22:59:04 +0700

    init1

---
+++ day1/main.py
@@ -0,0 +1,99 @@
+# # # name = input('Input Your Name : ')
+# # # print('Hello World ' + name)
+
+# # # age = int(input('Input Your Age : '))
+# # # if age > 20:
+# # #     print('Dewasa')
+
+# # # temperature = int(input('Input Room Temperature : '))
+# # # if temperature > 30:
+# # #     print('Warm')
+# # # if temperature < 30:
+# # #     print('Cold')
+
+# # if 5 > 2:
+# #     if 3 > 2:
+# #         print("Hello")
+# #         print("World")
+     
+# # 	# if 3 > 2:
+# # 	# 	print("Five is greater than two!")
+# #     #     print("Hello")
+      
+# # # if 5 > 2:
+# # #     if 3 > 2:
+# # #         print("Five is greater than two!")
+# # #         print("Hello")
+
+# # try:
+# #     temperature = int(input('Input the number : '))
+# #     if temperature > 0 and temperature < 20:
+# #         print('Cold')
+# #     elif temperature >= 20 and temperature < 35:
+# #         print('Warm')
+# #     elif temperature >= 35:
+# #         print('Hot')
+# #     else :
+# #         print('Frozen')
+# # except ValueError:
+# #     print('Invalid input. Please enter a valid integer.')
+
+# try:
+#     evenodd = int(input('Input the number : '))
+#     if evenodd % 2 != 0:
+#         print('Ganjil')
+#     elif evenodd % 2 == 0:
+#         print('Genap')
+# except Exception:
+#     print('Invalid input. Please enter a valid integer.')
+# evenodd = 
+# while i < 6:
+#   print(i)
+#   i += 1
+
+# print('correct') if 100 / 2 == 50 else print('false')
+
+# list_data = [1, 25, 50, 75]
+# if 100 in list_data:print('zzz')
+
+# data = [1,2,3,4]
+# for wow in data:
+#     print(wow)
+# data [2] = 10
+# print(data[3])
+# data[3] = 0
+# print(data[3])
+
+# word = ['derta', 'adalah', 'manusia']
+# for x in word:
+#     print(x[2])
+
+# word = [[1,2,[True, 'joko', ['fajri', 'amin']]],['derta']]
+# print(word [0])
+# print(word [0][2])
+# print(word [0][2][2][1][1])
+
+# Create a dictionary
+my_dict = {
+    "name": "John",
+    "age": 30,
+    "city": "New York"
+}
+
+# Accessing dictionary values
+print("Name:", my_dict["name"])
+print("Age:", my_dict["age"])
+print("City:", my_dict["city"])
+
+# Modifying dictionary values
+my_dict["age"] = 31
+
+# Adding a new key-value pair
+my_dict["country"] = "USA"
+
+# Removing a key-value pair
+del my_dict["city"]
+
+# Iterating through the dictionary
+for key, value in my_dict.items():
+    print(key, ":", value)
