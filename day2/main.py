c700c71d2c826f820b90feb121b27a410664e013
Al Amin Wicaksono <alaminwicaksono@gmail.com>
2023-10-13 00:09:51 +0700

    range with loop

--- day2/main.py
+++ day2/main.py
@@ -1,5 +1,14 @@
-print (list(range(10)))
+# print (list(range(10)))
 
-print(list(range(2,8)))
+# print(list(range(2,8)))
 
-print(tuple(range(2,20,3)))
+# print(tuple(range(2,20,3)))
+
+# for i in range(4):
+#     print(i)
+
+# for i in range(2,5):
+#     print(i)
+
+for i in range(2,10,2):
+    print(i)
