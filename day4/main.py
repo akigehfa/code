a079816c4576fe1a8283bd5454fa2dde43f90cd4
Al Amin Wicaksono <alaminwicaksono@gmail.com>
2023-10-14 21:00:15 +0700

    function

--- day4/main.py
+++ day4/main.py
@@ -1,10 +1,10 @@
-def sayHello():
-    # print('Bussiness logic here!')
-    return 'Hello return'
+# def sayHello():
+#     # print('Bussiness logic here!')
+#     return 'Hello return'
 
-sayHello()
-sayHelloVariable = sayHello()
-print(sayHelloVariable)
+# sayHello()
+# sayHelloVariable = sayHello()
+# print(sayHelloVariable)
 
 # def square(number):
 #     result = number * number
@@ -42,9 +42,70 @@ print(sayHelloVariable)
 # print(f"The factorial of {number} is {result}")
 
 
-def hello(name):
-    return 'Hello ' + name + '!'
+# def hello(name):
+#     return 'Hello ' + name + '!'
 
-helloAmin = hello('Fajri')
-print(helloAmin)
+# helloAmin = hello('Fajri')
+# print(helloAmin)
+
+# class Dog:
+#     def __init__(self, name, breed):
+#         self.name = name
+#         self.breed = breed
+
+#     def bark(self):
+#         print(f"{self.name} is barking")
+
+# my_dog = Dog("Fido", "Labrador")
+# my_dog.bark()
+
+# class MyClass:
+#     def __init__(self):
+#         self.public_var = 42
+#         self._protected_var = 23
+#         self.__private_var = 15
+
+# obj = MyClass()
+# print(obj.public_var)      # Accessible
+# print(obj._protected_var)  # Accessible, but conventionally considered protected
+# print(obj.__private_var)   # Name mangling, not directly accessible
+
+
+# def average(listInput):
+#     sum = 0
+#     for element in listInput:
+#         sum = sum + element
+#     return sum / len(listInput)
+# print(average([1,2,3,4,5]))
+
+# def maximum(listData):
+#     if not listData:
+#         return None
+#     maximum = listData[0]
+#     for number in listData:
+#         if number > maximum:
+#             maximum = number
+#     return maximum
+
+# print(maximum([12,32,43,13,75,42]))
+
+def add(a, b):
+    return a + b
+
+print(add(5, 2))
+
+def decrease(a, b):
+    return a - b
+
+print(decrease(9, 4))
+
+def divide(a, b):
+    return a / b
+
+print(divide(8, 2))
+
+def multiplication(a, b):
+    return a * b
+
+print(multiplication(78, 23))
 
