f9cee4a45e85b0f7df186c2c04251cf9e8bc6432
Al Amin Wicaksono <alaminwicaksono@gmail.com>
2023-10-17 18:24:35 +0700

    pypi and pip

--- day6/main.py
+++ day6/main.py
@@ -1,20 +1,40 @@
 import os
 
-while True:
-    # try:  
-        # os.system('clear')
+# while True:
+    
+#         os.system('clear')
 
-        print(' Welcome to The Club')
-        print('====================')
-        print('1. Input your name. ')
-        print('2. Exit.')
-        option = input('Input your options: ')
+        # print(' Welcome to The Club')
+        # print('====================')
+        # print('1. Input your name. ')
+        # print('2. Exit.')
+        # option = input('Input your options: ')
         
-        if option == '2':
-            print('Thanks for coming!')
-            break
+        # if option == '2':
+        #     print('Thanks for coming!')
+        #     break
 
-        name = input('Input: ')
-        print('\nHello, Mr.Mrs.', name + '\n')
-        print('=====================')
+        # name = input('Input: ')
+        # print('\nHello, Mr.Mrs.', name + '\n')
+        # print('=====================')
         
+import requests
+
+# Replace 'user' and 'pass' with your actual username and password
+url = 'https://httpbin.org/basic-auth/user/pass'
+response = requests.get(url, auth=('user', 'pass'))
+
+# Check the HTTP status code
+if response.status_code == 200:
+    print("Request was successful.")
+    print("Content-Type:", response.headers['content-type'])
+    print("Encoding:", response.encoding)
+    
+    # Print the response text
+    print(response.text)
+    
+    # Parse the JSON response
+    data = response.json()
+    print(data)
+else:
+    print("Request failed with status code:", response.status_code)
